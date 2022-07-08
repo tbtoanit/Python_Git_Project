8aa19ed50e43b701c446961dace2844cf3253b49
duyphuong1091 <nguyenduyphuong1091@gmail.com>
2022-07-08 20:48:54 +0700

    Phương thêm 1 đoạn code print() và sửa dòng 13 và sửa dòng 23,24

--- main.py
+++ main.py
@@ -10,7 +10,7 @@
 def kiem_tra_so_nguyen():
     count = 0
     while True:
-        s = input('Vui long nhap so') # s = "6.5"
+        s = input('Vui long nhap so: ') # s = "6.5"
         if s.isdigit(): #sẽ là True nếu chuỗi không chứa gì khác ngoài số
             print("Số nguyên")
             break
@@ -20,7 +20,12 @@ def kiem_tra_so_nguyen():
             if count == 3:
                 print("Bị khóa")
                 break
+            else:
+                print("Không bị khoá !")
 
 kiem_tra_so_nguyen()
 print("Chức năng của toan tran")
 print("Thêm chức năng thứ 2 của toàn trần")
+
+# thêm dòng code mới
+print("Thêm dòng code mới !")
