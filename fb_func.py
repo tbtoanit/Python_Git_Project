2c94fc1e28403b541646ee6d002862b0e85faaa5
HHanh2022 <honghanh.vothuy@gmail.com>
2022-07-10 17:20:37 +0700

    Hanh commit file

--- fb_func.py
+++ fb_func.py
@@ -3,3 +3,6 @@ def xac_thuc_email():
 
 def luu_thong_tin_nguoi_dung():
     print("Bỏ 150 dòng code vào function này")
+
+def luu_thong_tin_nguoi_dung():
+    print("Bỏ 120 dòng code vào function này")
