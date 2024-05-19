41d4dc12ab87fc80de2c39f665e32e10514cb827
tbtoanit <tbtoanit@gmail.com>
2024-05-19 16:36:09 +0700

    Develop according to email "Leader request zzz"

--- bt_function.py
+++ bt_function.py
@@ -19,5 +19,3 @@ def kt_snt(a):
             break #khi đã có 1 số chia hết, không còn số nguyên tố=> hệ thống ngừng=> thoát khỏi vòng lặp=>tiết kiệm Ram cho máy tính
     return nguyen_to
 
-#Toan Tran chỉnh sửa 1 đoạn code
-#abc
